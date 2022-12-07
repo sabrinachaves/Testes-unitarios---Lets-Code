1e70bd3597d7b77ec3fe97879601deb48df629e7
sabrinachaves <sabrinach08@gmail.com>
2022-12-06 21:41:15 -0300

    Testes de falhas do 'updatePassword'

--- TDD/tests/units/services/user-service.spec.js
+++ TDD/tests/units/services/user-service.spec.js
@@ -1,143 +1,188 @@
-const User = require('../../../src/models/User')
-const UserService = require('../../../src/services/user-service')
+const User = require("../../../src/models/User");
+const UserService = require("../../../src/services/user-service");
 
 class UserMock {
-    static async findOne({ email }) {
-        const users = {
-            'esdras@lets.com.br': {
-                _id: "63815037f041b254122338c3",
-                name: "Esdras Aguilar",
-                email: "esdras@lets.com",
-                password: "123456"
-            }
-        }
-        
-        if(users[email]) {
-            return users[email]
-        }
+  static async findOne({ email }) {
+    const users = {
+      "esdras@lets.com.br": {
+        _id: "63815037f041b254122338c3",
+        name: "Esdras Aguilar",
+        email: "esdras@lets.com",
+        password: "123456",
+      },
+    };
 
-        return null
+    if (users[email]) {
+      return users[email];
     }
 
-    // static async create({email}){
-    //     return true
-    // }
+    return null;
+  }
 
+  // static async create({email}){
+  //     return true
+  // }
 }
 
 describe('User Service "userExists"', () => {
-    it('Should return true if user exists', async () => {
-        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.findOne)
-        
-        const userExists = await UserService.userExists('esdras@lets.com.br')
+  it("Should return true if user exists", async () => {
+    jest.spyOn(User, "findOne").mockImplementationOnce(UserMock.findOne);
 
-        expect(userExists).toBe(true)
-    })
+    const userExists = await UserService.userExists("esdras@lets.com.br");
 
-    it('Should return false if user not found', async () => {
-        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.findOne)
-        
-        const userExists = await UserService.userExists('esdras.nao.existente@lets.com.br')
+    expect(userExists).toBe(true);
+  });
 
-        expect(userExists).toBe(false)
-    })
+  it("Should return false if user not found", async () => {
+    jest.spyOn(User, "findOne").mockImplementationOnce(UserMock.findOne);
 
-    it('Should capture if User model throws', async () => {
-        jest.spyOn(User, 'findOne').mockImplementationOnce(() => {
-            throw new Error('User model fails')
-        })
-        
-        try {
-            await UserService.userExists('esdras.nao.existente@lets.com.br')
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error)
-        }
-    })
-})
+    const userExists = await UserService.userExists(
+      "esdras.nao.existente@lets.com.br"
+    );
+
+    expect(userExists).toBe(false);
+  });
+
+  it("Should capture if User model throws", async () => {
+    jest.spyOn(User, "findOne").mockImplementationOnce(() => {
+      throw new Error("User model fails");
+    });
+
+    try {
+      await UserService.userExists("esdras.nao.existente@lets.com.br");
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+    }
+  });
+});
 
 describe('User Service "checkPassword"', () => {
-    it('Should returns false if password does not match', async () => {
-        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.findOne)
+  it("Should returns false if password does not match", async () => {
+    jest.spyOn(User, "findOne").mockImplementationOnce(UserMock.findOne);
 
-        const isValidPassword = await UserService.checkPassword('esdras@lets.com.br', '123455')
+    const isValidPassword = await UserService.checkPassword(
+      "esdras@lets.com.br",
+      "123455"
+    );
 
-        expect(isValidPassword).toBe(false)
-    }) 
+    expect(isValidPassword).toBe(false);
+  });
 
-    it('Should returns false user not found', async () => {
-        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.findOne)
+  it("Should returns false user not found", async () => {
+    jest.spyOn(User, "findOne").mockImplementationOnce(UserMock.findOne);
 
-        const isValidPassword = await UserService.checkPassword('invalid@lets.com.br', '123456')
+    const isValidPassword = await UserService.checkPassword(
+      "invalid@lets.com.br",
+      "123456"
+    );
 
-        expect(isValidPassword).toBe(false)
-    })
+    expect(isValidPassword).toBe(false);
+  });
 
-    it('Should returns true if password is valid', async () => {
-        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.findOne)
+  it("Should returns true if password is valid", async () => {
+    jest.spyOn(User, "findOne").mockImplementationOnce(UserMock.findOne);
 
-        const isValidPassword = await UserService.checkPassword('esdras@lets.com.br', '123456')
+    const isValidPassword = await UserService.checkPassword(
+      "esdras@lets.com.br",
+      "123456"
+    );
 
-        expect(isValidPassword).toBe(true)
-    })
-})
+    expect(isValidPassword).toBe(true);
+  });
+});
 
 describe('User Service "create"', () => {
-    //it('Should return false if user exist', )
-    
-    it('Should return true if create an user', async () =>  {
-        jest.spyOn(UserService, 'userExists').mockImplementationOnce(() => false)
+  //it('Should return false if user exist', )
 
-        jest.spyOn(User, 'create').mockImplementationOnce(()=> {
-            return {
-                "name": "alguem",
-                "email": "abc@abc.com",
-                "password": "triste",
-                "_id": "638ea57734bac791a9286581",
-                "__v": 0
-              }
-        })
+  it("Should return true if create an user", async () => {
+    jest.spyOn(UserService, "userExists").mockImplementationOnce(() => false);
 
-        const isCreateTrue = await UserService.create({email: 'abc@abc.com.br'})
+    jest.spyOn(User, "create").mockImplementationOnce(() => {
+      return {
+        name: "alguem",
+        email: "abc@abc.com",
+        password: "triste",
+        _id: "638ea57734bac791a9286581",
+        __v: 0,
+      };
+    });
 
-        expect(isCreateTrue).toMatchObject({
-            "name": "alguem",
-            "email": "abc@abc.com",
-            "password": "triste",
-            "_id": "638ea57734bac791a9286581",
-            "__v": 0
-          })
-    })
+    const isCreateTrue = await UserService.create({ email: "abc@abc.com.br" });
 
-    it('Should return false if create an user', async () =>  {
-        jest.spyOn(UserService, 'userExists').mockImplementationOnce(() => true)   
-        
+    expect(isCreateTrue).toMatchObject({
+      name: "alguem",
+      email: "abc@abc.com",
+      password: "triste",
+      _id: "638ea57734bac791a9286581",
+      __v: 0,
+    });
+  });
+
+  it("Should return false if create an user", async () => {
+    jest.spyOn(UserService, "userExists").mockImplementationOnce(() => true);
+
     try {
-        await UserService.create({email: 'abc@abc.com.br'})
-        console.log({ status: 404, message: 'User Already exists' })
-    } catch (error){
-        expect(error).toMatchObject( { status: 404, message: 'User Already exists' })
+      await UserService.create({ email: "abc@abc.com.br" });
+    } catch (error) {
+      expect(error).toMatchObject({
+        status: 404,
+        message: "User Already exists",
+      });
     }
-       
-    })
-})
+  });
+});
 
-describe('User service update Password',() => {
+describe("User service update Password", () => {
+  it("Should return a 'ok' message if it is a valid password", async () => {
+    jest.spyOn(UserService, "userExists").mockImplementationOnce(() => true);
+    jest.spyOn(User, "updateOne").mockImplementation(() => {
+      return {
+        message: "ok",
+      };
+    });
 
-    it('Should return true if it is a valid password', async() => {
-    jest.spyOn(UserService, 'userExists').mockImplementationOnce(( ) => true)
-    jest.spyOn(User, 'updateOne').mockImplementation(() => {
-        return {
-            "message": "ok"
-        }
-    })
-
-    const passwordChangedIsTrue = await UserService.updatePassword('name@email.com', 'teste1234', 'teste1234')
+    const passwordChangedIsTrue = await UserService.updatePassword(
+      "name@email.com",
+      "teste1234",
+      "teste1234"
+    );
 
     expect(passwordChangedIsTrue).toMatchObject({
-        "message": "ok"
-    })
-    })
+      message: "ok",
+    });
+  });
 
-    
+  it("Should return an error message if it is an invalid password", async () => {
+    jest.spyOn(UserService, "userExists").mockImplementationOnce(() => true);
 
-})
+    try {
+    await UserService.updatePassword(
+            "name@email.com",
+            "teste12345",
+            "teste123456"
+          );
+    } catch (error) {
+        expect(error).toMatchObject({
+            status: 400,
+            message: "Password does not match",
+    })
+    }
+  });
+
+  it("Should return an error message if user does not exist", async () => {
+    jest.spyOn(UserService, "userExists").mockImplementationOnce(() => false);
+
+      try {
+        await UserService.updatePassword(
+                "name@email.com",
+                "teste12345",
+                "teste12345"
+              );
+        } catch (error) {
+            expect(error).toMatchObject({
+                status: 404, 
+                message: 'User Not Found' ,
+        })
+        }
+  });
+});
